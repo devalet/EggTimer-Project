355a7423b33c3e6bd0d8f30b2d67b692b2639df5
devalet <amy@huddlesport.com.au>
2024-09-20 15:27:06 +1000

    Add Assets
    Updated folder to include assets

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle
+        
+        if hardness == "Soft" {
+            print(eggTimes)
+        } else if hardness == "Medium" {
+            print(eggTimes)
+        } else {
+            print(eggTimes)
+        }
+    }
+    
+}
